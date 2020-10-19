c4ddaf10476821656728c4af31fb40a6a93c8232
Edward Bebbington <edward.bebbington@intercity.technology>
2020-10-19 20:16:39 +0100

    wip 6

--- deps.ts
+++ deps.ts
@@ -1,6 +1,3 @@
 export { assertEquals } from "https://deno.land/std@0.74.0/testing/asserts.ts";
 export { deferred } from "https://deno.land/std@0.74.0/async/deferred.ts";
 export { delay } from "https://deno.land/std@0.74.0/async/mod.ts";
-export {
-  existsSync,
-} from "https://deno.land/std@0.74.0/fs/mod.ts";
