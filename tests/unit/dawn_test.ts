f448c8a08c66bb2f35ca3ee316ab4a500f2a512a
Eric Crooks <eric.crooks@gmail.com>
2020-10-20 07:00:30 -0400

    use rhum v2; export deno again for integration tests

--- tests/unit/dawn_test.ts
+++ tests/unit/dawn_test.ts
@@ -1,17 +1,17 @@
-import { Rhum } from "../deps.ts";
+import { Rhum2 } from "../deps.ts";
 import { Cinco } from "../../mod.ts";
 
-Rhum.testPlan("tests/unit/cinco_test.ts", () => {
-  Rhum.testSuite("assertUrlIs()", () => {
-    Rhum.testCase("Assertion should work", async () => {
+Rhum2.testPlan(() => {
+  Rhum2.testSuite("assertUrlIs()", () => {
+    Rhum2.testCase("Assertion should work", async () => {
       const cinco = new Cinco("https://chromestatus.com");
       await cinco.start();
       await cinco.assertUrlIs("https://chromestatus.com/features");
       await cinco.done();
     });
   });
-  Rhum.testSuite("assertSee()", () => {
-    Rhum.testCase("Assertion should work", async () => {
+  Rhum2.testSuite("assertSee()", () => {
+    Rhum2.testCase("Assertion should work", async () => {
       const cinco = new Cinco("https://chromestatus.com");
       await cinco.start();
       await cinco.assertSee("Chrome Platform Status");
@@ -19,5 +19,3 @@ Rhum.testPlan("tests/unit/cinco_test.ts", () => {
     });
   });
 });
-
-Rhum.run();
