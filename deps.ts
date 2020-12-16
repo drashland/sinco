6ef65c1120339e8ae078ce53a214935484e977aa
crookse <crookse@users.noreply.github.com>
2020-12-16 00:36:55 +0000

    Update dependencies and bump version numbers across repo

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { assertEquals } from "https://deno.land/std@0.79.0/testing/asserts.ts";
-export { readLines } from "https://deno.land/std@0.79.0/io/mod.ts";
+export { assertEquals } from "https://deno.land/std@0.81.0/testing/asserts.ts";
+export { readLines } from "https://deno.land/std@0.81.0/io/mod.ts";
