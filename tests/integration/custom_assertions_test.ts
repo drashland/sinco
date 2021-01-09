38421c209bb45ab22504b1e7c5460c72926a2340
Edward Bebbington <edward.bebbington@intercity.technology>
2021-01-09 14:26:48 +0000

    [#38-cleanup-in-assertions] Add custom assertions test from docs

---
+++ tests/integration/custom_assertions_test.ts
@@ -0,0 +1,10 @@
+import { HeadlessBrowser } from "../../mod.ts";
+
+Deno.test("My web app works as expected", async () => {
+  const Sinco = new HeadlessBrowser();
+  await Sinco.build();
+  await Sinco.goTo("https://chromestatus.com");
+  await Sinco.assertUrlIs("https://chromestatus.com/features");
+  await Sinco.assertSee("Chrome versions");
+  await Sinco.done();
+})
