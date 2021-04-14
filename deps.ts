bd3cb820244e1fdda63c0ab4b8f0d217836e9d2a
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-04-14 14:18:43 +0100

    fix import

--- deps.ts
+++ deps.ts
@@ -2,4 +2,4 @@ export { assertEquals } from "https://deno.land/std@0.93.0/testing/asserts.ts";
 export { readLines } from "https://deno.land/std@0.93.0/io/mod.ts";
 export { deferred } from "https://deno.land/std@0.93.0/async/deferred.ts";
 export type { Deferred } from "https://deno.land/std@0.93.0/async/deferred.ts";
-export { iter } from "https://deno.land/std@0.93.0/io/utils.ts"
+export { iter } from "https://deno.land/std@0.93.0/io/util.ts"
