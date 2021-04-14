e4abb780dff784ef4abbd9ee54c3de56b7701a63
crookse <crookse@users.noreply.github.com>
2021-04-14 00:51:30 +0000

    Update dependencies and bump version numbers across repo

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { assertEquals } from "https://deno.land/std@0.92.0/testing/asserts.ts";
-export { readLines } from "https://deno.land/std@0.92.0/io/mod.ts";
-export { deferred } from "https://deno.land/std@0.92.0/async/deferred.ts";
-export type { Deferred } from "https://deno.land/std@0.92.0/async/deferred.ts";
+export { assertEquals } from "https://deno.land/std@0.93.0/testing/asserts.ts";
+export { readLines } from "https://deno.land/std@0.93.0/io/mod.ts";
+export { deferred } from "https://deno.land/std@0.93.0/async/deferred.ts";
+export type { Deferred } from "https://deno.land/std@0.93.0/async/deferred.ts";
