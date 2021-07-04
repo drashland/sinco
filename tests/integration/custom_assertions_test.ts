fb20bd6f4762f35bb74e839beeb5842714af06d1
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-07-04 23:21:41 +0100

    fix tests

--- tests/integration/custom_assertions_test.ts
+++ tests/integration/custom_assertions_test.ts
@@ -3,7 +3,7 @@ import { ChromeClient, FirefoxClient } from "../../mod.ts";
 Deno.test("Chrome: Assertions - Tutorial for this feature in the docs should work", async () => {
   const Sinco = await ChromeClient.build();
   await Sinco.goTo("https://drash.land");
-  await Sinco.assertUrlIs("https://drash.land");
+  await Sinco.assertUrlIs("https://drash.land/");
   await Sinco.assertSee("Develop With Confidence");
   await Sinco.done();
 });
@@ -11,7 +11,7 @@ Deno.test("Chrome: Assertions - Tutorial for this feature in the docs should wor
 Deno.test("Firefox: Assertions - Tutorial for this feature in the docs should work", async () => {
   const Sinco = await FirefoxClient.build();
   await Sinco.goTo("https://drash.land");
-  await Sinco.assertUrlIs("https://drash.land");
+  await Sinco.assertUrlIs("https://drash.land/");
   await Sinco.assertSee("Develop With Confidence");
   await Sinco.done();
 });
