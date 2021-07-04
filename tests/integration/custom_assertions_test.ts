f32cda4ad9a962759dc35aec32f549d1e5b4f649
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-07-04 23:18:53 +0100

    fix tests

--- tests/integration/custom_assertions_test.ts
+++ tests/integration/custom_assertions_test.ts
@@ -2,16 +2,16 @@ import { ChromeClient, FirefoxClient } from "../../mod.ts";
 
 Deno.test("Chrome: Assertions - Tutorial for this feature in the docs should work", async () => {
   const Sinco = await ChromeClient.build();
-  await Sinco.goTo("https://chromestatus.com");
-  await Sinco.assertUrlIs("https://chromestatus.com/features");
-  await Sinco.assertSee("Chrome versions");
+  await Sinco.goTo("https://drash.land");
+  await Sinco.assertUrlIs("https://drash.land");
+  await Sinco.assertSee("Develop With Confidence");
   await Sinco.done();
 });
 
 Deno.test("Firefox: Assertions - Tutorial for this feature in the docs should work", async () => {
   const Sinco = await FirefoxClient.build();
-  await Sinco.goTo("https://chromestatus.com");
-  await Sinco.assertUrlIs("https://chromestatus.com/features");
-  await Sinco.assertSee("Chrome versions");
+  await Sinco.goTo("https://drash.land");
+  await Sinco.assertUrlIs("https://drash.land");
+  await Sinco.assertSee("Develop With Confidence");
   await Sinco.done();
 });
