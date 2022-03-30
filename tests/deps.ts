8be3106088af2f967d7187f06c05eed8961d6980
crookse <crookse@users.noreply.github.com>
2022-03-30 01:09:38 +0000

    Update dependencies and bump version numbers across repo

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,2 +1,2 @@
-export { Rhum } from "https://deno.land/x/rhum@v1.1.12/mod.ts";
-export * as Drash from "https://deno.land/x/drash@v2.5.1/mod.ts";
+export { Rhum } from "https://deno.land/x/rhum@v1.1.13/mod.ts";
+export * as Drash from "https://deno.land/x/drash@v2.5.4/mod.ts";
