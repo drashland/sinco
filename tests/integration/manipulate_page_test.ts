336132067bf09254cf26d9919b171433491970ce
Edward Bebbington <47337480+ebebbington@users.noreply.github.com>
2022-04-27 17:02:24 +0100

    chore: Update tests files (#116)
    * chore: Update tests files
    
    * fix: tests fail because span is not in document
    
    Co-authored-by: Eric Crooks <eric.crooks@gmail.com>
    Co-authored-by: Eric Crooks <crookse@users.noreply.github.com>

--- tests/integration/manipulate_page_test.ts
+++ tests/integration/manipulate_page_test.ts
@@ -4,54 +4,55 @@ import { buildFor } from "../../mod.ts";
 import { browserList } from "../browser_list.ts";
 
 for (const browserItem of browserList) {
-  Deno.test(browserItem.name + ": Manipulate Webpage", async () => {
-    const { browser, page } = await buildFor(browserItem.name);
-    await page.location("https://drash.land");
-
-    const updatedBody = await page.evaluate(() => {
-      // deno-lint-ignore no-undef
-      const prevBody = document.body.children.length;
-      // deno-lint-ignore no-undef
-      const newEl = document.createElement("p");
-      // deno-lint-ignore no-undef
-      document.body.appendChild(newEl);
-      // deno-lint-ignore no-undef
-      return prevBody === document.body.children.length - 1;
-    });
-    assertEquals(updatedBody, true);
-
-    await browser.close();
-  });
-
-  Deno.test(
-    browserItem.name +
-      ": Evaluating a script - Tutorial for this feature in the documentation works",
-    async () => {
+  Deno.test(browserItem.name, async (t) => {
+    await t.step("Manipulate Webpage", async () => {
       const { browser, page } = await buildFor(browserItem.name);
       await page.location("https://drash.land");
-      const pageTitle = await page.evaluate(() => {
+
+      const updatedBody = await page.evaluate(() => {
         // deno-lint-ignore no-undef
-        return document.title;
+        const prevBody = document.body.children.length;
+        // deno-lint-ignore no-undef
+        const newEl = document.createElement("p");
+        // deno-lint-ignore no-undef
+        document.body.appendChild(newEl);
+        // deno-lint-ignore no-undef
+        return prevBody === document.body.children.length - 1;
       });
-      const sum = await page.evaluate(`1 + 10`);
-      const oldBodyLength = await page.evaluate(() => {
-        // deno-lint-ignore no-undef
-        return document.body.children.length;
-      });
-      const newBodyLength = await page.evaluate(() => {
-        // deno-lint-ignore no-undef
-        const p = document.createElement("p");
-        p.textContent = "Hello world!";
-        // deno-lint-ignore no-undef
-        document.body.appendChild(p);
-        // deno-lint-ignore no-undef
-        return document.body.children.length;
-      });
+      assertEquals(updatedBody, true);
+
       await browser.close();
-      assertEquals(pageTitle, "Drash Land");
-      assertEquals(sum, 11);
-      assertEquals(oldBodyLength, 3);
-      assertEquals(newBodyLength, 4);
-    },
-  );
+    });
+
+    await t.step(
+      "Evaluating a script - Tutorial for this feature in the documentation works",
+      async () => {
+        const { browser, page } = await buildFor(browserItem.name);
+        await page.location("https://drash.land");
+        const pageTitle = await page.evaluate(() => {
+          // deno-lint-ignore no-undef
+          return document.title;
+        });
+        const sum = await page.evaluate(`1 + 10`);
+        const oldBodyLength = await page.evaluate(() => {
+          // deno-lint-ignore no-undef
+          return document.body.children.length;
+        });
+        const newBodyLength = await page.evaluate(() => {
+          // deno-lint-ignore no-undef
+          const p = document.createElement("p");
+          p.textContent = "Hello world!";
+          // deno-lint-ignore no-undef
+          document.body.appendChild(p);
+          // deno-lint-ignore no-undef
+          return document.body.children.length;
+        });
+        await browser.close();
+        assertEquals(pageTitle, "Drash Land");
+        assertEquals(sum, 11);
+        assertEquals(oldBodyLength, 3);
+        assertEquals(newBodyLength, 4);
+      },
+    );
+  });
 }
